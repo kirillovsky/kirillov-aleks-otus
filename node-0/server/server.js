885253bba9edc9f5e81d1d6ce43dec7941acf02c
Кириллов Александр Валерьевич <AVKirillov2.SBT@sberbank.ru>
2019-01-04 14:25:33 +0300

    implement server

---
+++ node-0/server/server.js
@@ -0,0 +1,30 @@
+require('log-timestamp');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 9000;
+const timeoutInMills = 100;
+
+const server = http.createServer((request, response) => {
+  const bodyChunks = [];
+
+  request.on('data', (data) => bodyChunks.push(data));
+  request.on('end', () => {
+    const body = Buffer.concat(bodyChunks).toString();
+    console.log(`Received request (Method: ${request.method}, URL: ${request.url}, Body: ${body})`);
+    defferEcho(response, body, timeoutInMills);
+  });
+});
+
+function defferEcho(response, requestString, timeoutInMills) {
+  setTimeout(() => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end(`Your request - '${requestString}' received`);
+    console.log('Echo response was sent to client');
+  }, timeoutInMills);
+}
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
